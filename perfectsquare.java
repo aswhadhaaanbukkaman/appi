4dd73cf26408d29906d777e26c6e731fc00fbaa4
aswhadhaaanbukkaman <aswhadhaa111@gmail.com>
2016-09-07 11:07:45 +0530

    Create perfectsquare.java

---
+++ perfectsquare.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+public class perfectsquare {
+ public static void main(String[] args) {
+      
+
+Scanner s= new Scanner(System.in); 
+double number; 
+
+System.out.println("Enter number >");
+
+number= s.nextDouble();
+
+System.out.println(Math.sqrt(number));
+
+
+
+
+
+    }
+
+}
